0527aa4682e628a687957c488f79317c785ed953
Flopy Sueiro <flopysueiro1@gmail.com>
2026-08-05 20:17:11 -0300

    Test orden

--- src/App.tsx
+++ src/App.tsx
@@ -36,9 +36,10 @@ export default function App() {
             <Suspense fallback={<Loader compact />}><AmbientScene /></Suspense>
           </div>
           <Hero />
+          <About />
           <ScrollyStory />
           <KineticBand />
-          <About />
+          
           <Technologies />
           <Projects />
           <Experience />
